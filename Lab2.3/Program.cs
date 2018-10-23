f87886c2f978e67ac3595aaa01bb2e2163c49ae6
Vladimir Klimov <vladimirklimoff@yandex.ru>
2018-10-23 19:28:50 +0500

    init

--- Lab2.3/Program.cs
+++ Lab2.3/Program.cs
@@ -10,6 +10,42 @@ namespace Lab2._3
     {
         static void Main(string[] args)
         {
+            int number = 0;
+            Console.WriteLine("Введите натуральное число больше 1");
+
+            try
+            {
+                number = int.Parse(Console.ReadLine());
+                if (number <= 1)
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Не натуральное число или число, равное 1");
+                return;
+            }
+
+            bool isNumberPrime = true;
+
+            for (int i = 2; i * i <= number; i = 2 * i - 1)
+            {
+                if (number % i == 0)
+                {
+                    isNumberPrime = false;
+                    break;
+                }
+            }
+            
+            if (isNumberPrime)
+            {
+                Console.WriteLine($"Число {number} простое");
+            }
+            else
+            {
+                Console.WriteLine($"Число {number} не простое");
+            }
         }
     }
 }
